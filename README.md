f9e4e6932f2f578cb256c440e302017a43cd7487
Imanol Aranzadi <iiaranzadi@gmail.com>
2015-02-20 21:23:06 -0800

    uu

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 #Twilio App for Food Location in West Sacremento
+
+- create a virtual enviroment
+- pip install flask
+- pip install twilio
+- python run.py
--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from twilio.rest import TwilioRestClient
 
 # Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "ACb280962d4d018c20a1ae8fb4d2eaa3ce"
-auth_token  = "42c39c5abb5da02d4ad2e2a413339045"
+account_sid = ""
+auth_token  = ""
 client = TwilioRestClient(account_sid, auth_token)
 
 message = client.messages.create(body="Hey dude, Where is my food?",
